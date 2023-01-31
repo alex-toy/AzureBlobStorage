0dfeb479ffdbec281c9d20617ffc22cdf69ed514
alex-toy <alexei.80@hotmail.fr>
2023-01-31 20:29:54 +0100

    blob controller

---
+++ BlobStorage/BlobSampleApp/Controllers/BlobFilesController.cs
@@ -0,0 +1,24 @@
+﻿using BlobStorageExample.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlobSampleApp.Controllers
+{
+    public class BlobFilesController : Controller
+    {
+        private readonly IBlobService _blobService;
+
+        public BlobFilesController(IBlobService blobService)
+        {
+            _blobService = blobService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<string> files = await _blobService.GetAll("mycontainer");
+            return View(files);
+        }
+    }
+}
